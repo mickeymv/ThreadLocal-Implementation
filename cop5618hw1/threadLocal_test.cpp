8d9514dcc69dd4c6f7c2e67365bb96bdb83e67e5
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2016-10-05 16:50:58 -0400

    Test cases with different data-types.

--- cop5618hw1/threadLocal_test.cpp
+++ cop5618hw1/threadLocal_test.cpp
@@ -8,29 +8,89 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 #include "threadLocal.h"
 
-//void is_main_thread();
+int test_threadLocal1(int num_threads);
+int test_threadLocal2(int num_threads);
+int test_threadLocal3(int num_threads);
+
 
 int test_threadLocal() {
     //should run test suite and return number of errors.
-	int errors = 0;
-    cop5618::threadLocal<std::string> threadLocalVariable1;
+	int numOfErrors = 0;
+    cop5618::threadLocal<std::string> threadLocalStringVariable;
     cop5618::threadLocal<int> threadLocalVariable2;
-    threadLocalVariable1.set("M");
-    std::thread th1(&cop5618::threadLocal<std::string>::set,&threadLocalVariable1,"1");
-    std::thread th2(&cop5618::threadLocal<std::string>::set,&threadLocalVariable1,"2");
-    std::thread th3(&cop5618::threadLocal<std::string>::set,&threadLocalVariable1,"3");
-    std::thread th4(&cop5618::threadLocal<int>::set,&threadLocalVariable2,4);
-    std::thread th5(&cop5618::threadLocal<int>::set,&threadLocalVariable2,5);
-//std::thread th2(&cop5618::threadLocal<std::string>::get,&threadLocalVariable);
-//	std::thread th(&cop5618::threadLocal<std::string>::get,&threadLocalVariable);
-//th = std::thread(&cop5618::threadLocal<std::string>::get,&threadLocalVariable);
-	th1.join();
-	th2.join();
-    th3.join();
-    th4.join();
-    th5.join();
-    threadLocalVariable1.get();
-    return errors;
+    threadLocalStringVariable.set("M");
+    numOfErrors += test_threadLocal1(10);
+    numOfErrors += test_threadLocal2(10);
+    //numOfErrors += test_threadLocal3(10);
+    threadLocalStringVariable.get();
+    return numOfErrors;
 }
+
+
+/**
+ * test_threadLocal1 tests having int variables
+ *
+ */
+int test_threadLocal1(const int num_threads) {
+    std::vector<std::thread> threads;
+    cop5618::threadLocal<int> threadLocalIntVariable;
+    
+    std::atomic<int> sequencer(1);
+    
+    for (int t = 0; t < num_threads; t++) {
+        threads.push_back(
+                          std::thread(
+                                      [&sequencer, &threadLocalIntVariable]() mutable ->void
+                                      {
+                                          {
+                                              threadLocalIntVariable.set(sequencer++);
+                                          }
+                                      }
+                                      )
+                          );
+        
+    }
+    
+    //join all the threads
+    std::for_each(threads.begin(), threads.end(),
+        std::mem_fn(&std::thread::join));
+    
+    return 0;
+}
+
+/**
+ * test_threadLocal2 tests having string variables
+ *
+ */
+int test_threadLocal2(const int num_threads) {
+    std::vector<std::thread> threads;
+    cop5618::threadLocal<std::string> threadLocalStringVariable;
+    
+    std::atomic<int> sequencer(1);
+    
+    for (int t = 0; t < num_threads; t++) {
+        threads.push_back(
+                          std::thread(
+                                      [&sequencer, &threadLocalStringVariable]() mutable ->void
+                                      {
+                                          {
+                                              threadLocalStringVariable.set(std::to_string(sequencer++));
+                                          }
+                                      }
+                                      )
+                          );
+        
+    }
+    
+    //join all the threads
+    std::for_each(threads.begin(), threads.end(),
+                  std::mem_fn(&std::thread::join));
+    
+    return 0;
+}
+
+
+
